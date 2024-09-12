a147d44f3a2d43f94fd1996b26aba61a07c754a1
VishalKumarSain <vishaldhoria007@gmail.com>
2024-09-12 09:44:48 +0530

    test one

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className='app'>
-        
-        <textarea className='textarea'>
-                {/* <p>Enter your text here...</p> */}
-        </textarea>
-        <div className='preview'>
-                <div className='loading'></div>
-        </div>
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
 
+
+  const showText = (e) => {
+    const val = e.target.value;
+    if (e.key == "Enter" && val?.trim()) {
+        console.log("val===", val);
+
+      setData(val);
+      const preview = document.getElementsByClassName("preview")[0]
+      const textarea = document.getElementsByClassName("textarea")[0]
+      preview.innerHTML = val;
+      textarea.innerHTML = ""
+      
+    }
+  };
+  return (
+    <div className="app">
+      <textarea className="textarea" name="textarea" onKeyDown={showText}>
+        {/* <p>Enter your text here...</p> */}
+      </textarea>
+      <div className="preview">
+        <div className="loading">{loading ? "Loading..." : ""}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
