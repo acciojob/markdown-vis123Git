69865a486d8e7af3a69d0d1297a818f1c3753f50
VishalKumarSain <vishaldhoria007@gmail.com>
2024-09-12 08:58:17 +0530

    basic

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react'
 
+function App() {
+  return (
+    <div className='app'>
+        
+        <textarea className='textarea'>
+                {/* <p>Enter your text here...</p> */}
+        </textarea>
+        <div className='preview'>
+                <div className='loading'></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default App
